fe34574a9d0c77da0456c3ec6d28dae400c2247c
alocam30 <99086842+alocam30@users.noreply.github.com>
2022-09-14 12:02:12 -0700

    Update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-console.log(Test);
+//variable for the first form field to be in focus, with a flashing cursor
+const nameElement = document.getElementById('name');
+nameElement.focus();
+
+
+//job role section
+//when a change is detected display/hide the "text" field
+const jobRole = document.getElementById('title');
+
+const otherJobRole = document.getElementById('other-job-role');
+
+    jobRole.addEventListener("change", e => {
+        if (e.target.value === "other"){
+        otherJobRole.style.display = '';
+        } else {
+            otherJobRole.style.display = 'none';
+        }
+
+    });
+
