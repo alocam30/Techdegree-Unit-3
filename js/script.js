a832c9ad03be511d8c4ffbac32e9d0f388bc2f11
alocam30 <99086842+alocam30@users.noreply.github.com>
2022-09-19 08:22:11 -0700

    Update

--- js/script.js
+++ js/script.js
@@ -24,27 +24,28 @@ const shirtDesign = document.getElementById('design');
 const shirtColor = document.getElementById('color');
 shirtColor.disabled = true;
 
-let jsPuns = document.querySelectorAll('[data-theme="js puns"]');
-    
-let  heartJS = document.querySelectorAll('[data-theme="heart js"]');
 
 shirtDesign.addEventListener("change", e => {
     shirtColor.disabled = false;
-    let eventValue = e.target.value;
+    
+    for (let i = 0; i < shirtColor.length; i++ ) {
+        let eventValue = e.target.value;
+
+        let jsPuns = document.querySelectorAll('[data-theme="js puns"]');
+    
+        let  heartJS = document.querySelectorAll('[data-theme="heart js"]');
 
          if (eventValue === jsPuns) {
-            for (let i = 0; i < shirtColor.length; i++ ) {
-            jsPuns.hidden = false;
-            heartJS.hidden = true;
-         } }  else {
-            for (let i = 0; i < shirtColor.length; i++ ) 
-            heartJS.hidden = false;
-            jsPuns.hidden = true;
-         };
-
+            
+            jsPuns[i].hidden = false;
+            heartJS[i].hidden = true;
+         }  else {
+            jsPuns[i].hidden = true;
+            heartJS[i].hidden = false;
          }
 
-);
+     }
+    });
 
 //register for activities section
 
@@ -73,33 +74,107 @@ const payment = document.getElementById('payment');
 const creditCard = document.getElementById('credit-card');
 
 const paypal = document.getElementById('paypal');
-paypal.hidden = true;
 
 const bitcoin = document.getElementById('bitcoin');
+
+paypal.hidden = true;
 bitcoin.hidden = true;
 
- payment.setAttribute('value.children', 'value.children.selected'); 
+payment.setAttribute('value.children', 'value.children.selected'); 
 
 payment.addEventListener("change", e => {
     let paymentClicked = e.target.value;
-    if (paymentClicked === creditCard ){
+    if (paymentClicked === creditCard.id ){
         creditCard.hidden = false;
         paypal.hidden = true;
         bitcoin.hidden = true;
-    } else if (paymentClicked === paypal) {
+    } else if (paymentClicked === paypal.id) {
         paypal.hidden = false;
         creditCard.hidden = true;
         bitcoin.hidden = true;
-    } else if (paymentClicked === bitcoin ){
+    } else (paymentClicked === bitcoin.id)
+    {
         bitcoin.hidden = false;
         paypal.hidden = true;
         creditCard.hidden = true;
     }
+});
 
+////form validation section
+
+nameElement;
+const email = document.getElementById('email');
+activities;
+const card = document.getElementById('cc-num');
+const zipcode = document.getElementById('zip');
+const cvv = document.getElementById('cvv');
+const form = document.querySelector('form');
+
+function nameTest (){
+    let nameInput = nameElement.value;
+    let nameResult = /^[a-zA-z]+ ?[a-zA-Z]*? ?[a-zA-Z]*?$/.test(nameInput);
+    return nameResult;
+};
+
+//email validation
+
+let emailResult;
+
+function emailTest(){
+    let emailInput = email.value;
+    emailResult = /^[^@]+@[^@.]+\.[a-z]+$/i.test(emailInput);
+    return emailResult;
+};
+
+// activities 
+let activitiesResult;
+
+function activitiesTest(){
+    let activitiesInput = totalCost > 0;
+    return activitiesInput;
+};
+
+//card validation
+let cardResult;
+
+function cardTest(card){
+    let cardInput = card.value;
+    cardResult = /^[0-9]{13,16}$/.test(cardInput);
+    return cardResult;
+};
+
+
+//zipcode validation
+let zipcodeResult;
+
+function zipcodeTest(zipcode){
+    let zipcodeInput = zipcode.value;
+    zipcodeResult = /^\d{5}$/.test(zipcodeInput);
+    return zipcodeResult;
+};
+
+
+//cvv validation
+
+let cvvResult;
+
+function cvvTest(cvv){
+    let cvvInput = cvv.value;
+    cvvResult = /^\d{3}$/.test(cvvInput);
+    return cvvResult;
+};
+
+
+
+//accessibility
+const activitiesCheckbox =  document.querySelectorAll('[type="checkbox"]');
+
+for (let i = 0; i < activitiesCheckbox.length; i++) {
+activitiesCheckbox[1].addEventListener("focus", e => {
+
+});
+activitiesCheckbox[1].addEventListener("blur", e => {
 
 });
 
-//form validation
-
-//accessibility
-
+}
